69fc29eb62606e3149ad343cacd3aafb92edd300
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-04 15:54:13 +0400

    Inherit from IllegalArgumentException (#5)

--- src/main/java/com/greenpas/user/UsernameAlreadyExistsException.java
+++ src/main/java/com/greenpas/user/UsernameAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.greenpas.user;
 
-public class UsernameAlreadyExistsException extends RuntimeException {
+public class UsernameAlreadyExistsException extends IllegalArgumentException {
 
     public UsernameAlreadyExistsException(String username) {
         super(String.format("User %s already exists", username));
