8dad5e2b641bccb329f37d49ab9637671725ba65
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-04 21:51:03 +0400

    Add ChangePasswordCommand class (#5)

---
+++ src/main/java/com/greenpas/user/web/ChangePasswordCommand.java
@@ -0,0 +1,20 @@
+package com.greenpas.user.web;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.springframework.util.Assert;
+
+public class ChangePasswordCommand {
+
+    private final String password;
+
+    @JsonCreator
+    public ChangePasswordCommand(@JsonProperty("password") String password) {
+        Assert.hasText(password, "Password must not be blank");
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
