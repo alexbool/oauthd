246b7f2956b2504814bd84b3da71519a696c2bea
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-05 17:09:16 +0400

    Use JdbcTokenStore (#3)

--- src/main/resources/sql/oauth-schema.sql
+++ src/main/resources/sql/oauth-schema.sql
@@ -4,4 +4,21 @@ CREATE TABLE users (
     password VARCHAR(256) NOT NULL,
     authorities VARCHAR(1024) NOT NULL,
     deleted TINYINT);
-    
+
+-- Access tokens
+CREATE TABLE oauth_access_token (
+    token_id VARCHAR(256),
+    token LONGVARBINARY,
+    authentication_id VARCHAR(256),
+    user_name VARCHAR(256),
+    client_id VARCHAR(256),
+    authentication LONGVARBINARY,
+    refresh_token VARCHAR(256)
+);
+
+-- Refresh tokens
+CREATE TABLE oauth_refresh_token (
+    token_id VARCHAR(256),
+    token LONGVARBINARY,
+    authentication LONGVARBINARY
+);
