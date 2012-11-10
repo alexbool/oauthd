4fdbea0ed51c118be01ecc1640becbdd33e3de08
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-10 19:09:21 +0400

    Better exception handling (#16)

--- src/main/java/com/alexbool/client/web/ClientEndpoint.java
+++ src/main/java/com/alexbool/client/web/ClientEndpoint.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.provider.BaseClientDetails;
+import org.springframework.security.oauth2.provider.ClientAlreadyExistsException;
 import org.springframework.security.oauth2.provider.ClientRegistrationService;
+import org.springframework.security.oauth2.provider.NoSuchClientException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import com.alexbool.util.StatusCodeException;
 
 @RequestMapping(value = "client", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ClientEndpoint {
@@ -22,12 +25,19 @@ public class ClientEndpoint {
     public void register(@RequestBody RegisterClientCommand cmd) {
         BaseClientDetails client = new BaseClientDetails(cmd.getClientId(), "", "",
                 "authorization_code,refresh_token,implicit,password", "client_general");
-        clientRegistrationService.addClientDetails(client);
+        try {
+            clientRegistrationService.addClientDetails(client);
+        } catch (ClientAlreadyExistsException ex) {
+            throw new StatusCodeException(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+        }
     }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.DELETE)
     public void delete(Principal principal) {
-        clientRegistrationService.removeClientDetails(principal.getName());
+        try {
+            clientRegistrationService.removeClientDetails(principal.getName());
+        } catch (NoSuchClientException ex) {
+        }
     }
 }
