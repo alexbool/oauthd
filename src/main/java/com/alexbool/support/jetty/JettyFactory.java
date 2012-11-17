e4a3460e3b2a887959f167010f61914d1681416d
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-18 00:32:40 +0400

    Add Access-Control-Allow-Origin header for cross-domain AJAX requests

--- src/main/java/com/alexbool/support/jetty/JettyFactory.java
+++ src/main/java/com/alexbool/support/jetty/JettyFactory.java
@@ -14,6 +14,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
+import com.alexbool.util.AccessControlAllowOriginFilter;
 
 public class JettyFactory implements ApplicationContextAware {
 
@@ -31,6 +32,7 @@ public class JettyFactory implements ApplicationContextAware {
         
         ctx.addFilter(new FilterHolder(new DelegatingFilterProxy("springSecurityFilterChain", applicationContext)), "/*",
                 EnumSet.of(DispatcherType.REQUEST));
+        ctx.addFilter(new FilterHolder(new AccessControlAllowOriginFilter()), "/*", EnumSet.of(DispatcherType.REQUEST));
 
         server.setHandler(ctx);
         return server;
---
+++ src/main/java/com/alexbool/util/AccessControlAllowOriginFilter.java
@@ -0,0 +1,29 @@
+package com.alexbool.util;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class AccessControlAllowOriginFilter implements Filter {
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+            ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(req, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+}
