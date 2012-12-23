e0a993b32180ddb1a7942ffde1786524f323e2c6
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-23 23:17:09 +0400

    Use try-with-resources to close app context correctly

--- src/main/java/com/alexbool/oauth/Main.java
+++ src/main/java/com/alexbool/oauth/Main.java
@@ -6,16 +6,17 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        XmlWebApplicationContext ctx = new XmlWebApplicationContext();
-        ctx.setConfigLocation("classpath:application-context.xml");
-        ctx.refresh();
-        ctx.registerShutdownHook();
-        
-        Server apiJetty = ctx.getBean("apiJetty", Server.class);
-        Server adminJetty = ctx.getBean("adminJetty", Server.class);
-        apiJetty.start();
-        adminJetty.start();
-        apiJetty.join();
-        adminJetty.join();
+        try (XmlWebApplicationContext ctx = new XmlWebApplicationContext()) {
+            ctx.setConfigLocation("classpath:application-context.xml");
+            ctx.refresh();
+            ctx.registerShutdownHook();
+            
+            Server apiJetty = ctx.getBean("apiJetty", Server.class);
+            Server adminJetty = ctx.getBean("adminJetty", Server.class);
+            apiJetty.start();
+            adminJetty.start();
+            apiJetty.join();
+            adminJetty.join();
+        }
     }
 }
