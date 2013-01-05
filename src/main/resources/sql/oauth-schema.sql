14cfcfc286b2dd5664a0956beafe24473e6b31cd
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-06 00:55:44 +0400

    Trivial: fmt

--- src/main/resources/sql/oauth-schema.sql
+++ src/main/resources/sql/oauth-schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE users (
     username VARCHAR(256) NOT NULL PRIMARY KEY,
     password VARCHAR(256) NOT NULL,
     authorities VARCHAR(1024) NOT NULL,
-    deleted TINYINT);
+    deleted TINYINT
+);
 
 -- Client details
 CREATE TABLE oauth_client_details (
