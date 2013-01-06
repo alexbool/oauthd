d0ed4ad43449811f16530f0ab957bab4bfa24ca8
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-06 14:00:43 +0400

    Remove serialVersionUID

--- src/main/java/com/alexbool/oauth/user/User.java
+++ src/main/java/com/alexbool/oauth/user/User.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class User implements UserDetails {
-    
-    private static final long serialVersionUID = -3066380991869635502L;
 
     private final String username;
     private final String password;
