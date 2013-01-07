4382448b04b9320fb40c700f9a89a38d574838af
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-07 21:41:00 +0400

    CC

--- src/main/java/com/alexbool/oauth/user/web/UserInfo.java
+++ src/main/java/com/alexbool/oauth/user/web/UserInfo.java
@@ -1,6 +1,10 @@
 package com.alexbool.oauth.user.web;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.security.core.GrantedAuthority;
 
