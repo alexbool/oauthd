11424e947d3c6184682777934f3f7221544313b9
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-07 17:57:51 +0400

    Fmt

--- src/main/resources/sql/oauth-schema.sql
+++ src/main/resources/sql/oauth-schema.sql
@@ -1,39 +1,39 @@
 -- User details
 CREATE TABLE users (
-    username VARCHAR(256) NOT NULL PRIMARY KEY,
-    password VARCHAR(256) NOT NULL,
-    authorities VARCHAR(1024) NOT NULL,
-    deleted TINYINT
+    username    VARCHAR(256)    NOT NULL PRIMARY KEY,
+    password    VARCHAR(256)    NOT NULL,
+    authorities VARCHAR(1024)   NOT NULL,
+    deleted     TINYINT
 );
 
 -- Client details
 CREATE TABLE oauth_client_details (
-    client_id VARCHAR(256) PRIMARY KEY,
-    resource_ids VARCHAR(256),
-    client_secret VARCHAR(256),
-    scope VARCHAR(256),
-    authorized_grant_types VARCHAR(256),
+    client_id               VARCHAR(256) PRIMARY KEY,
+    resource_ids            VARCHAR(256),
+    client_secret           VARCHAR(256),
+    scope                   VARCHAR(256),
+    authorized_grant_types  VARCHAR(256),
     web_server_redirect_uri VARCHAR(256),
-    authorities VARCHAR(256),
-    access_token_validity INTEGER,
-    refresh_token_validity INTEGER,
-    additional_information VARCHAR(4096)
+    authorities             VARCHAR(256),
+    access_token_validity   INTEGER,
+    refresh_token_validity  INTEGER,
+    additional_information  VARCHAR(4096)
 );
 
 -- Access tokens
 CREATE TABLE oauth_access_token (
-    token_id VARCHAR(256) PRIMARY KEY,
-    token LONGVARBINARY,
-    authentication_id VARCHAR(256),
-    user_name VARCHAR(256),
-    client_id VARCHAR(256),
-    authentication LONGVARBINARY,
-    refresh_token VARCHAR(256)
+    token_id            VARCHAR(256) PRIMARY KEY,
+    token               LONGVARBINARY,
+    authentication_id   VARCHAR(256),
+    user_name           VARCHAR(256),
+    client_id           VARCHAR(256),
+    authentication      LONGVARBINARY,
+    refresh_token       VARCHAR(256)
 );
 
 -- Refresh tokens
 CREATE TABLE oauth_refresh_token (
-    token_id VARCHAR(256) PRIMARY KEY,
-    token LONGVARBINARY,
-    authentication LONGVARBINARY
+    token_id        VARCHAR(256) PRIMARY KEY,
+    token           LONGVARBINARY,
+    authentication  LONGVARBINARY
 );
